36ad629b9a97fb90e0c47e424cdc30dc4722f8d7
Miguel Equina <miguel.equina@ymail.com>
2025-03-18 11:37:42 -0600

    Update install_rec_printer.sh

--- install_rec_printer.sh
+++ install_rec_printer.sh
@@ -2,7 +2,7 @@
 
 echo "INSTALLING PRINTERS: "
 
-/usr/sbin/lpadmin -p "TW_Recife_Printer" -v "lpd://10.141.105.129/" -D "Recife Office Printer (Kyocera KM-2820)" -L "Recife, Brazil" -P "/Library/Printers/PPDs/Contents/Resources/en.lproj/Kyocera KM-2820.PPD" -E
+/usr/sbin/lpadmin -p "TW_Recife_Printer" -v "lpd://$IP-ADDRESS/" -D "Recife Office Printer (Kyocera KM-2820)" -L "Recife, Brazil" -P "/Library/Printers/PPDs/Contents/Resources/en.lproj/Kyocera KM-2820.PPD" -E
 
 echo “[[[PRINTER]]] FINISHED”
 
