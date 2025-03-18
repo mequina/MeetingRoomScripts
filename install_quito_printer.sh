f1d75c4741926be15b4e7d09425239784899f3b2
Miguel Equina <miguel.equina@ymail.com>
2025-03-18 11:37:28 -0600

    Update install_quito_printer.sh

--- install_quito_printer.sh
+++ install_quito_printer.sh
@@ -2,9 +2,9 @@
 
 echo "INSTALLING PRINTERS: "
 
-/usr/sbin/lpadmin -p Impresora_Pequena -v socket://10.71.33.250/ -D "Impresora HP Pequena y a Color" -L "Cerca de la Salida Emergencia" -P "/Library/Printers/PPDs/Contents/Resources/HP LaserJet CP 1025nw.gz" -E
+/usr/sbin/lpadmin -p Impresora_Pequena -v socket://$IP-ADDRESS/ -D "Impresora HP Pequena y a Color" -L "Cerca de la Salida Emergencia" -P "/Library/Printers/PPDs/Contents/Resources/HP LaserJet CP 1025nw.gz" -E
 
-/usr/sbin/lpadmin -p Impresora_Grande -v socket://10.71.33.251/ -D "Impresora Grande y Copiadora Xerox" -L "Cerca de la Salida Emergencia" -P "/Library/Printers/PPDs/Contents/Resources/Xerox WorkCentre 5325.gz" -E
+/usr/sbin/lpadmin -p Impresora_Grande -v socket://$IP-ADDRESS/ -D "Impresora Grande y Copiadora Xerox" -L "Cerca de la Salida Emergencia" -P "/Library/Printers/PPDs/Contents/Resources/Xerox WorkCentre 5325.gz" -E
 
 /usr/sbin/lpadmin -d Impresora_Grande
 
